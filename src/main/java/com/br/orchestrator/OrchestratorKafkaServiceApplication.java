67e4aae69de14066b55a7f582105eb6ca22a574c
breno.mcosta <breno.mcosta@pbh.gov.br>
2024-01-18 15:12:51 -0300

    up

---
+++ src/main/java/com/br/orchestrator/OrchestratorKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorKafkaServiceApplication.class, args);
+	}
+
+}
